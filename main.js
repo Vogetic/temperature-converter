d3005807bacf95d20e3bc4e37faef8aa3c7e4735
Vuong <uhhbs@student.kit.edu>
2026-08-06 02:52:50 +0200

    Add conversion algorithm to event listener

--- main.js
+++ main.js
@@ -39,6 +39,57 @@ toUnit.addEventListener("change", () => {
   }
 });
 
-if (!button.disabled) {
-  button.addEventListener("click", () => {});
+button.addEventListener("click", () => {
+  if (!button.disabled) {
+    let result;
+
+    if (fromUnit.value == "Celsius") {
+      result = celsiusConverter();
+    } else if (fromUnit.value == "Fahrenheit") {
+      result = fahrenheitConverter();
+    } else {
+      result = kelvinConverter();
+    }
+
+    result = Math.round(result * 100) / 100;
+
+    document.querySelector(".result").textContent =
+      temperatureInput.value +
+      " " +
+      fromUnit.value +
+      " is " +
+      result +
+      " " +
+      toUnit.value;
+  }
+});
+
+function celsiusConverter() {
+  if (toUnit.value == "Fahrenheit") {
+    return Number(temperatureInput.value) * (9 / 5) + 32;
+  } else if (toUnit.value == "Kelvin") {
+    return Number(temperatureInput.value) + 273.15;
+  } else {
+    return Number(temperatureInput.value);
+  }
+}
+
+function fahrenheitConverter() {
+  if (toUnit.value == "Celsius") {
+    return (Number(temperatureInput.value) - 32) * (5 / 9);
+  } else if (toUnit.value == "Kelvin") {
+    return (Number(temperatureInput.value) + 459.67) * (5 / 9);
+  } else {
+    return Number(temperatureInput.value);
+  }
+}
+
+function kelvinConverter() {
+  if (toUnit.value == "Fahrenheit") {
+    return Number(temperatureInput.value) * (9 / 5) - 459.67;
+  } else if (toUnit.value == "Celsius") {
+    return Number(temperatureInput.value) - 273.15;
+  } else {
+    return Number(temperatureInput.value);
+  }
 }
